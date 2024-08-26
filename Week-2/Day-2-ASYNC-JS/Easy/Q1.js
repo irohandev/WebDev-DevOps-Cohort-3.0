5c1daba94bed4129a3f4c14200ea79f6f6f1fe39
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-08-26 13:27:00 +0530

    Assignment Update

--- Week-2/Day-2-ASYNC-JS/Easy/Q1.js
+++ Week-2/Day-2-ASYNC-JS/Easy/Q1.js
@@ -1,2 +1,11 @@
 // Create a counter in JavaScript
 // We have already covered this in the second lesson, but as an easy recap try to code a counter in Javascript It should go up as time goes by in intervals of 1 second
+
+let ctr = 0
+function Stopwatch() {
+    console.log(ctr); 
+
+    ctr = ctr + 1;
+}
+
+setInterval(Stopwatch, 1000);
