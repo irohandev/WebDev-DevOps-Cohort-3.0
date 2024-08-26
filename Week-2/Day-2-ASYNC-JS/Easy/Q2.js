9a9d110e0d8333aab85a77f4e7cd0e724caf2b12
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-08-26 13:35:50 +0530

    assignment Update

--- Week-2/Day-2-ASYNC-JS/Easy/Q2.js
+++ Week-2/Day-2-ASYNC-JS/Easy/Q2.js
@@ -2,3 +2,15 @@
 // Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 
 // (Hint: setTimeout)
+
+let ctr = 0
+function Stopwatch() {
+    console.log(ctr); 
+    ctr = ctr + 1;
+
+    setTimeout(Stopwatch,1000);
+}
+
+setTimeout(Stopwatch, 1000);
+
+//Kind of recursion ke jaise we are implementing setTimeout as asetInterval function.
