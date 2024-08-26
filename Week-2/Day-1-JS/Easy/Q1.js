04c8f8bbbbd8aae3650df1c5d6eeb879d50031a6
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-08-26 12:25:27 +0530

    Updated

--- Week-2/Day-1-JS/Easy/Q1.js
+++ Week-2/Day-1-JS/Easy/Q1.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const sortedstr1 = str1.sort()
+  const sortedstr2 = str2.sort()
+  if (sortedstr1==sortedstr2){
+    console.log("True");
+    
+  }
+  else{
+    console.log("False");
+    
+  }
 }
 
 module.exports = isAnagram;
