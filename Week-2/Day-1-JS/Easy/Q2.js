fcf1a51a0931954e5fa066b650ccc44fe6cd8857
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-08-26 20:17:45 +0530

    Assignment Update

--- Week-2/Day-1-JS/Easy/Q2.js
+++ Week-2/Day-1-JS/Easy/Q2.js
@@ -12,9 +12,29 @@
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
+function calculateTotalSpentByCategory(transactions){
+  let totalSpentByCategory = [];      //new array initialize kiye 
+  for(let i=0; i<transactions.length; i++){
+      const category = totalSpentByCategory.find(item => item.category === transactions[i].category);
 
-function calculateTotalSpentByCategory(transactions) {
-    return [];
+      if (category){
+          category.totalSpent += transactions[i].price;
+      }
+      else{
+          totalSpentByCategory.push({
+              category: transactions[i].category,
+              totalSpent: transactions[i].price
+          });
+      }
   }
-  
-  module.exports = calculateTotalSpentByCategory;
+  return totalSpentByCategory;
+}
+const transactions = [      //inputs
+  { id: 1, timestamp: 1656076800000, price: 10, category: 'Food', itemName: 'Pizza' },
+  { id: 2, timestamp: 1656076800000, price: 5, category: 'Food', itemName: 'Burger' },
+  { id: 3, timestamp: 1656076800000, price: 20, category: 'Entertainment', itemName: 'Movie Ticket' },
+  { id: 4, timestamp: 1656076800000, price: 15, category: 'Food', itemName: 'Pasta' },
+  { id: 5, timestamp: 1656076800000, price: 30, category: 'Entertainment', itemName: 'Concert Ticket' }
+];
+
+console.log(calculateTotalSpentByCategory(transactions));
