d562912c7d08489cb763dd73289c1c6dc5276f21
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-08-27 20:48:19 +0530

    Offline Task Update

---
+++ Week-2/Offline-Videos/Callback-Hell-Promisified.js
@@ -0,0 +1,36 @@
+// Q: Write code using promisified using 
+// 1.logs hi after 1 second
+// 2.logs hello 3 seconds after step 1
+// 3.logs heyy there 5 seconds after step 2
+
+
+function setTimeoutPromisified(duration){           //we made a promisified version of setTimeout 
+	return new Promise(function (resolve) {
+		setTimeout(resolve, duration);
+	});
+}
+
+setTimeoutPromisified(1000).then(function(){
+    console.log("Hi");
+    setTimeoutPromisified(3000).then(function(){
+        console.log("Hello");
+        setTimeoutPromisified(5000).then(function(){
+            console.log("Heyy");
+        })        
+    })
+})
+
+
+//Promise Chaning - Promise Chaining is a something by which we may initialize another promise inside our .then() method and accordingly we may execute our results. The function inside then captures the value returned by the previous promise.
+
+//Now doing the same thing using Promise Chaning method of Promisified Callback Hell:
+
+setTimeoutPromisified(1000).then(function(){
+    console.log("Hi");
+    return setTimeoutPromisified(3000)
+}).then(function(){
+    console.log("Hello");
+    return setTimeoutPromisified(5000)
+}).then(function(){
+    console.log("Heyy");    
+})
