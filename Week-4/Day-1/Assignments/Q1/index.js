56e3b79f50b51c4d6ce74c159f91188fb1e1b35e
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-08-31 13:31:52 +0530

    Week-4 Updated

---
+++ Week-4/Day-1/Assignments/Q1/index.js
@@ -0,0 +1,10 @@
+/*
+Assignments #1 - Create a cli
+
+Create a `command line interface` that lets the user specify a file path and the nodejs process counts the number of words inside it.
+
+Input - node index.js /Users/kirat/file.txt
+Output - You have 10 words in this file
+
+Command - `node index.js count_words filePath`
+*/
