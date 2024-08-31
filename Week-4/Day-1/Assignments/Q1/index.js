5aca39c6b3b1902e68a4a83e503a0c6d6b8f0dfe
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-08-31 17:04:51 +0530

    Week-4 Update

--- Week-4/Day-1/Assignments/Q1/index.js
+++ Week-4/Day-1/Assignments/Q1/index.js
@@ -8,3 +8,28 @@ Output - You have 10 words in this file
 
 Command - `node index.js count_words filePath`
 */
+
+const fs = require('fs');       //inetrnal module needed
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('counter')      //name of the program 
+  .description('CLI to do file based tasks')        //description og program likke woh krta kya hai
+  .version('0.8.0');            //version the program
+
+program.command('count')            //yeh program bnaye hai jo..count command bnaye hai jo number of words ko count krta hai     
+  .description('Count the number of lines in a file')       //yeh count ka description 
+  .argument('<file>', 'file to count')          //yeh woh argument jo function ko chhaiye woh count command ko execute krne ke liye chahiye
+  .action((file) => {           //ab yeh uska action ki like woh count command execute hua toh karna kya hai
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const lines = data.split('\n').length;
+        console.log(`There are ${lines} lines in ${file}`);
+      }
+    });
+  });
+  
+program.parse();
