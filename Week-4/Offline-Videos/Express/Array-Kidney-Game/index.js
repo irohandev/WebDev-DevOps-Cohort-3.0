6a2c16dc9edb0bca22afe2a7d74de61fc325c290
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-09-02 21:37:31 +0530

    Update

---
+++ Week-4/Offline-Videos/Express/Array-Kidney-Game/index.js
@@ -0,0 +1,14 @@
+/*
+Question : Your logic is like a doctor
+Learn by doing, lets create an in memory hospital
+
+You need to create 4 routes (4 things that the hospital can do)
+
+GET - User can check how many kidneys they have and their health
+POST - User can add a new kidney
+PUT - User can replace a kidney, make it healthy
+DELETE - User can remove a kidney
+
+What should happen if they try to delete when there are no kidneys?
+What should happen if they try to make a kidney healthy when all are already healthy?
+*/
--- Week-4/Offline-Videos/Express/Q2-Array-Kidney-Game/index.js
+++
@@ -1,9 +0,0 @@
-/*
-Learn by doing, lets create an in memory hospital - 
-
-You need to create 4 routes (4 things that the hospital can do)
-1. GET - User can check how many kidneys they have and their health
-2. POST - User can add a new kidney
-3. PUT - User can replace a kidney, make it healthy
-4. DELETE - User can remove a kidney
-*/
