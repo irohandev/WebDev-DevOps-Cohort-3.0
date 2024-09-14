66dec81fdbf5489b45605562d1816b518bc11f23
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-09-14 12:39:51 +0530

    Week-6 Update

---
+++ Week-6-AUTHENTICATION/Day-2-AUTH-CONNECTING-FE-TO-BE/01_Codes/01/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const JWT_SECRET = "randomabc"
+
+app.use(express.json())
+
+
+const users = []
+
+app.post("/signup", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+
+    users.push({
+        username:username,
+        password:password
+    });
+    res.json({
+        meassage:"Signed in successfully!"
+    });
+})
+app.post("/signin", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser =null;
+
+    for (let i = 0; i < users.length; i++){
+        if(users[i].username === username && users[i].password === password){
+            foundUser = users[i]
+        }        
+    }
+
+    if(!foundUser){
+        res.json({
+            message:"Invalid Credentials"
+        })
+    }
+    else{
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+    }
+
+})
+app.get("/me", function(req, res){
+
+})
+
+app.listen(3000);
