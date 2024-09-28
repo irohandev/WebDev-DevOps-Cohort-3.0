c01e3b020c9a8a105a94c251f22e7a13c370d472
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-09-28 13:31:36 +0530

    Week-7 Updated

--- Week-7-DATABASES-&-MONGODB/Day-2-PASSWORD-ZODS/Lecture_Codes/Todo/index.js
+++ Week-7-DATABASES-&-MONGODB/Day-2-PASSWORD-ZODS/Lecture_Codes/Todo/index.js
@@ -13,11 +13,20 @@ app.use(express.json())                         //body ko parse krne k liye chah
 
 app.post("/signup", async function(req, res){
 
-    const requiredBody = z.object({             //idhar zod use kiye hai
-        email : z.string(),
-        name : z.string(),
-        password : z.string()
+    const requiredBody = z.object({             //idhar zod use kiye hai..jisse woh teen input lega jo ki teeno string hone chahiye 
+        email : z.string().min(3).max(100).email(),                     //zod ka schema yeh hai here we multiple input validation things..this one tell ki minimum 3 characters hone chahiye max 100 and a email should be there
+        name : z.string().min(3).max(100),
+        password : z.string().min(3).max(100)
     }) 
+    const parsedDataSuccess = requiredBody.safeParse(req.body);         //yeh data ko check kr rha ki input kiya hua data zod ke schema k accrding hai ya nahi
+
+    if(!parsedDataSuccess.success){                     //If data is not correct then yeh response return kr do
+        res.json({
+            message: "Incorrect Format",
+            error: parsedDataSuccess.error              //yeh return kr dega ki user jis format mein input kr rha uspe error kya hai...jisse woh resignin/signup kre sahi format mein
+        })
+        return
+    }
 
     const email = req.body.email;               //yaha body pe req ja rha hai isliye eisko parse krna hai 
     const password = req.body.password;
