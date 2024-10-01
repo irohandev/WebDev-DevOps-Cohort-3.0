1818fdb88f0ce928e3fa472e5fa20a78b6a53731
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-10-01 20:21:42 +0530

    Week-8 Update

--- Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/index.js
+++ Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/index.js
@@ -9,20 +9,20 @@ app.post("/user/signup", function(req, res){
 
 app.post("/user/signin", function(req, res){
     res.json({
-        message: "Signin endpoint"
+        message: "Signup endpoint"
     })
 })
 
 
 app.get("/user/purchases", function(req, res){           //this routes for user ne jo purchase kiya hua hai uske liye
     res.json({
-        message: "Signin endpoint"
+        message: "Signup endpoint"
     })
 })
 
 app.post("/course/purchase", function(req, res){          //this routes for user jo purchase krega courses
     res.json({
-        message: "Signin endpoint"
+        message: "Signup endpoint"
     })
 })
 
