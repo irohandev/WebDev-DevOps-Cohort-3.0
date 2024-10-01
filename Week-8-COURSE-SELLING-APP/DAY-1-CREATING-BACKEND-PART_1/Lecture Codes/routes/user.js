95825736d19aca2730d5948593d55d2cda361194
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-10-01 20:27:55 +0530

    Week-8 Update

--- Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/routes/user.js
+++ Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/routes/user.js
@@ -1,17 +1,21 @@
-app.post("/user/signup", function(req, res){
+import { Router } from "express"        //new way of importing router from express
+const userRouter = Router();
+
+
+userRouter.post("/signup", function(req, res){      //ab userRouter bana chuke hai aur routing kiye hai toh we don't have access of app so userRouter use krenge uske jgah pe 
     res.json({
         message: "Signup endpoint"
     })
 })
 
-app.post("/user/signin", function(req, res){
+userRouter.post("/signin", function(req, res){
     res.json({
         message: "Signup endpoint"
     })
 })
 
 
-app.get("/user/purchases", function(req, res){           //this routes for user ne jo purchase kiya hua hai uske liye
+userRouter.get("/purchases", function(req, res){           //this routes for user ne jo purchase kiya hua hai uske liye
     res.json({
         message: "Signup endpoint"
     })
