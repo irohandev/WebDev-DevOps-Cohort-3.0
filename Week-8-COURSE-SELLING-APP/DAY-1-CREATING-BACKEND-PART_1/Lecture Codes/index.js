900c175426c2cb0c774cd03146e816fca06a9a6c
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-10-01 22:29:15 +0530

    Week-8 Update

--- Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/index.js
+++ Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/index.js
@@ -1,11 +1,11 @@
-const  express = require ("express")
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
 const { userRouter } = require("./routes/user")
 const {  courseRouter } = require("./routes/course")
 const { adminRouter } = require("./routes/admin")
 
-const app = express();
 
 app.use("/user", userRouter)        //yaha prefix daal diye ki "/user" se jo bhi req ayega woh userRouter ke pass jyega 
 app.use("/course", courseRouter)    //same here router ke prefix ko pehle hi define kr diye hai
