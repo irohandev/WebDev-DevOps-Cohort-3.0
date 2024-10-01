c80aac5a933e4bfd1e278018d0793957e6c401f8
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-10-01 20:35:27 +0530

    Week-8 Update

--- Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/index.js
+++ Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/index.js
@@ -1,4 +1,9 @@
 const  express = require ("express")
+const { userRouter } = require("./routes/user")
+const {  courseRouter } = require("./routes/course")
 const app = express();
 
+
+app.use("/user", userRouter)
+app.use("/course", courseRouter)
 app.listen(3000);
--- Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/routes/course.js
+++ Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/routes/course.js
@@ -1,11 +1,20 @@
-app.post("/course/purchase", function(req, res){          //this routes for user jo purchase krega courses
+import { Router } from "express"
+
+const courseRouter = Router();
+
+
+courseRouter.post("/purchase", function(req, res){          //this routes for user jo purchase krega courses
     res.json({
         message: "Signup endpoint"
     })
 })
 
-app.get("/courses", function(req, res){
+courseRouter.get("/", function(req, res){
     res.json({
         message: "Signup endpoint"
     })
 })
+
+module.exports = {
+    courseRouter: courseRouter
+}
