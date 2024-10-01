4249adf44c8ac9f672380a2e0f80a7cefec55b2e
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-10-01 20:22:13 +0530

    Week-8 Update

--- Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/index.js
+++ Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/index.js
@@ -1,24 +1,6 @@
 const  express = require ("express")
 const app = express();
 
-app.post("/user/signup", function(req, res){
-    res.json({
-        message: "Signup endpoint"
-    })
-})
-
-app.post("/user/signin", function(req, res){
-    res.json({
-        message: "Signup endpoint"
-    })
-})
-
-
-app.get("/user/purchases", function(req, res){           //this routes for user ne jo purchase kiya hua hai uske liye
-    res.json({
-        message: "Signup endpoint"
-    })
-})
 
 app.post("/course/purchase", function(req, res){          //this routes for user jo purchase krega courses
     res.json({
---
+++ Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/routes/user.js
@@ -0,0 +1,18 @@
+app.post("/user/signup", function(req, res){
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+app.post("/user/signin", function(req, res){
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+
+app.get("/user/purchases", function(req, res){           //this routes for user ne jo purchase kiya hua hai uske liye
+    res.json({
+        message: "Signup endpoint"
+    })
+})
