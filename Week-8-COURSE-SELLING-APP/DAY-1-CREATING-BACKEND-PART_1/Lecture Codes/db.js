c99562280191731cd6a8c3b039d35d6cce22cf7b
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-10-02 21:48:08 +0530

    Week-8 Update

--- Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/db.js
+++ Week-8-COURSE-SELLING-APP/DAY-1-CREATING-BACKEND-PART_1/Lecture Codes/db.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;                            
 const ObjectId = mongoose.ObjectId; 
 
-
 const userSchema = new Schema({
     email: {type: String, unique: true},
     password: String,
