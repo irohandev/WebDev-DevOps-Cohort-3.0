4b6cc2d1be2d949326bc8e4f91115806997b32ac
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-10-06 10:21:56 +0530

    Week-8 Update

--- Week-8-COURSE-SELLING-APP/DAY-2-CREATING-BACKEND-PART_2/Lecture Codes/routes/user.js
+++ Week-8-COURSE-SELLING-APP/DAY-2-CREATING-BACKEND-PART_2/Lecture Codes/routes/user.js
@@ -1,9 +1,50 @@
-const { Router } = require("express")        
+/*
+No need to do this as of now onwards!
+// Import express module
+const express = require("express");
+// Create a new Router instance for user routes
+const Router = express.Router;
+*/
+
+// Import the Router object from the express module to create route handlers
+const {Router} = require("express");
+
+// Create a new instance of the Router for defining user-related routes
 const userRouter = Router();
 
-userRouter.post("/signup", function(req, res){      //ab userRouter bana chuke hai aur routing kiye hai toh we don't have access of app so userRouter use krenge uske jgah pe 
+//import the required dependencies 
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const z = require("zod");
+
+
+//Post router for user to Signup
+userRouter.post("/signup", function(req, res){      
+
+    //input validation 
+    const requiredBody = z.object({             
+        email : z.string().min(3).max(100).email(),                     
+        name : z.string().min(3).max(100),
+        password : z.string().min(3).max(100)
+    }) 
+
+    // Parse the request body using the requireBody.safeParse() method to validate the data format
+    // "safe" parsing (doesn't throw error if validation fails)
+    const parsedDataSuccess = requiredBody.safeParse(req.body);         
+
+    //If data is not correct then yeh response return kr do
+    if(!parsedDataSuccess.success){                     
+        res.json({
+            message: "Incorrect Format",
+            error: parsedDataSuccess.error
+        })
+        return
+    }
+
+    // Extract validated email, password, firstName, and lastName from the request body
+    const {email, password, firstName, lastName} = req.body;
     res.json({
-        message: "Signup endpoint"
+        message: "Signed up Successfull"
     })
 })
 
