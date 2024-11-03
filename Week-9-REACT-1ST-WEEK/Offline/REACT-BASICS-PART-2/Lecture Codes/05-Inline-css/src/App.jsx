2811a8552131814db57a917a81b3d721b9d1e51c
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-11-03 21:34:48 +0530

    Week-9 Update

--- Week-9-REACT-1ST-WEEK/Offline/REACT-BASICS-PART-2/Lecture Codes/05-Inline-css/src/App.jsx
+++ Week-9-REACT-1ST-WEEK/Offline/REACT-BASICS-PART-2/Lecture Codes/05-Inline-css/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+// Create a function component named App that will be rendered in the root element
 function App() {
-  const [count, setCount] = useState(0)
-
+  // Return the JSX that will be rendered in the browser
   return (
-    <>
+      // Create a div element with JSX
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          {/* Render the MyComponent component */}
+          <MyComponent />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+// Create a MyComponent component that will render a div element with inline CSS
+function MyComponent() {
+  
+// Return JSX that will be rendered
+  return (
+      // Create a div element with inline CSS
+      <div style={{ backgroundColor: "blue", color: "white", padding: 10, borderRadius: 20 }}>
+          Hello, World!
+      </div>
+  );
+}
+
+// Export the App component as the default export to be used in other files or components
+export default App;
