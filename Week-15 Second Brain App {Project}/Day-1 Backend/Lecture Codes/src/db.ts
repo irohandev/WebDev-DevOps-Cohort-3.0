22c0261a89d11a3a6cc310466c31b020323015df
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-12-19 23:00:21 +0530

    Week-15 Update

--- Week-15 Second Brain App {Project}/Day-1 Backend/Lecture Codes/src/db.ts
+++ Week-15 Second Brain App {Project}/Day-1 Backend/Lecture Codes/src/db.ts
@@ -3,7 +3,7 @@ import mongoose, { model, Schema } from "mongoose";
 
 // Connecting to the MongoDB database using a connection string
 // Replace "password12345" with your actual database password for security
-mongoose.connect("mongodb+srv://Admin:password12345@cluster1.wm8zj.mongodb.net/second_brain");
+mongoose.connect("Your Connection String");
 
 // Defining a schema for the 'User' collection
 // Each user will have a unique 'username' and a 'password'
