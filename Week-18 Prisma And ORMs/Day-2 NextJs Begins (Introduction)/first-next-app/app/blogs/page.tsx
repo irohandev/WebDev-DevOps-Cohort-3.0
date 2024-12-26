6d2bc9e3a92b5beb833008cc9409369a2c3f4e19
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-12-26 19:15:19 +0530

    Week-18 Update

---
+++ Week-18 Prisma And ORMs/Day-2 NextJs Begins (Introduction)/first-next-app/app/blogs/page.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+
+async function getBlogs (){
+
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+    return response.data;
+}   
+
+export default async function Blogs(){
+
+    const blogs = await getBlogs();
+
+return <div>
+        {blogs.map((blog: Itodo) => <Todo title={blog.title} completed={blog.completed} />)}
+    </div>
+}
+
+interface Itodo {
+    title : String;
+    completed : boolean;
+}
+
+function Todo({title, completed} : Itodo){
+    return (
+        <div>
+            {title} {completed ? "done!" : "not done"}
+        </div>
+
+    )
+
+}
---
+++ Week-18 Prisma And ORMs/Day-2 NextJs Begins (Introduction)/first-next-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        First Next.js App - Todo Display App!
+      </h1>
+    </div>
+  );
+}
