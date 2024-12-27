4210d1b6f8cc08052e891378f654827e8b477b22
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2024-12-27 20:13:21 +0530

    Week-19 Update

--- Week-19 NextJs/Day-1 NextJs Continuation/Lecture Codes/02_backend_route_nextapp/app/api/v1/user/details/route.ts
+++ Week-19 NextJs/Day-1 NextJs Continuation/Lecture Codes/02_backend_route_nextapp/app/api/v1/user/details/route.ts
@@ -1,24 +1,27 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"; // Importing NextResponse for creating API responses
 
-export function GET(){
+// Function to handle GET requests
+export function GET() {
     return NextResponse.json({
         user: "Rohan Dev Singh",
         email: "rohan@gmail.com"
-    })
+    });
 }
-export function POST(){
+
+// Function to handle POST requests
+export function POST() {
     return NextResponse.json({
         user: "Rohan Dev",
         email: "rohandev@gmail.com"
-    })
+    });
 }
 
-
-
 /*
-Notes
-Default export jab ek se jyda fucntion hote toh anhi krte hai woh normal export hota ..
-aur jb default nhi hota hai toh usko import kuch aise krte hai ---- import {GET} from '.api/v1/user'
-but jb ek fucntion rhega toh usko default krenge then usko import kuch aisa krnge --- import GET from ".api/v1/user"
+Notes:
+When there are multiple functions in a module, the default export cannot be used. 
+In such cases, we use named exports and import them like this: 
+--- import { GET } from '.api/v1/user'
 
+However, if there is only one function, we can use default export and import it like this: 
+--- import GET from ".api/v1/user"
 */
